6e7cc5b012422b77763c14fe5a84ecb28054bcfd
netra2127 <netra200021kcbdr@gmail.com>
2024-08-17 23:47:57 +0100

    Image Filtering and Transformation

---
+++ Assignment-1/04_ImageFilteringTransformation.py
@@ -0,0 +1,17 @@
+# Image Filtering and Transformation.
+# Task: Implement a function to perform image Filtering and Transformation that includes:
+# 1. Filtering: Apply a Gaussian blur to the image to reduce noise and smooth the image. 
+# 2. Transformation: Apply a rotation to the image by a specified angle. 
+
+# For Gaussian blur, you can create a Gaussian kernel and apply it using convolution: https://en.wikipedia.org/wiki/Gaussian_blur
+# For rotation, compute the rotation matrix and use cv2.warpAffine: https://theailearner.com/tag/cv2-warpaffine/
+
+# G(x,y) = 1/2*PI*Sigma^2 * exp(- x^2 + y^2/2*sigma^2)
+
+
+
+
+
+
+
+
