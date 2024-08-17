4515163c7d6bb0984a1f9b864ab80f0b43ed55c8
netra2127 <netra200021kcbdr@gmail.com>
2024-08-17 23:48:33 +0100

    Appyling the normalization techniques from scratch

---
+++ Assignment-1/05_Normalization.py
@@ -0,0 +1,4 @@
+# Apply different Image Normalization technique from scratch and Visualize the effect. 
+# - Do normalization -- see effect on min, max, mean, std value. 
+# - See changes in pixel Distribution.
+# - 
