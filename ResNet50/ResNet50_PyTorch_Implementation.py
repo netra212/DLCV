5d15844171f53381c8cd5358971ea643bd759315
netr212 <netra200021kcbdr@gmail.com>
2025-02-26 15:28:36 +0000

    Building an ResNet Models from scratch in PyTorch

--- ResNet50/ResNet50_PyTorch_Implementation.py
+++ ResNet50/ResNet50_PyTorch_Implementation.py
@@ -56,11 +56,66 @@ class BottleNeck(nn.Module):
 
         out += identity # Adding the shortcut connection before an activation function. 
         out = my_relu(out) 
-        
+
 
 # Building an ResNet50 Architecture. 
 class ResNet50(nn.Module):
 
     def __init__(self, num_classes=15):
         super(ResNet50, self).__init__()
-        self.in_channels = 64 
+
+        self.in_channels = 64  # Initial input channels. 
+
+        # Initial Convolution Layer. 
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = my_relu
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        # Residual blocks (BottleNeck Layers).
+        self.layer1 = self._make_layer(64, 3, stride=1)
+        self.layer2 = self._make_layer(128, 4, stride=2)
+        self.layer3 = self._make_layer(256, 6, stride=2)
+        self.layer4 = self._make_layer(512, 3, stride=2)
+
+        # Global Average Pooling & Fully Connected Layer. 
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * 4, num_classes)
+    
+    def _make_layer(self, out_channels, blocks, stride):
+        downsample=None
+        if stride != 1 or self.in_channels != out_channels * BottleNeck.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.in_channels, out_channels * BottleNeck.expansion, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels*BottleNeck.expansion)
+            )
+        
+        layers = []
+        layers.append(BottleNeck(self.in_channels, out_channels, stride, downsample))
+        self.in_channels = out_channels * BottleNeck.expansion # Update the input channels. 
+
+        for _ in range(1, blocks): # block =3, 4, 6, 3
+            layers.append(BottleNeck(self.in_channels, out_channels))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+        
+        return x
+
+# Instantiate the Model. 
+model = ResNet50(num_classes=15)
+print(model)
